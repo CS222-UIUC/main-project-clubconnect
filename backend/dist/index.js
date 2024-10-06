ff8cb566b43e48f60bee67c0ee15d55b571b9d89
Matt Marsico <mjmarsico@hotmail.com>
2024-10-06 16:00:37 -0500

    Created project structure

---
+++ backend/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function main() {
+}
+// keep this function call here. We don't really need to have a main function but makes the code structuring a bit easier
+main();
---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+function main() {
+  
+}
+
+// keep this function call here. We don't really need to have a main function but makes the code structuring a bit easier
+main();
+
